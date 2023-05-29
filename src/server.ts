cdbfca79bfb37a69f79c513ac41d650c314e3113
jubairzaman <jubair.zaman01@gmail.com>
2023-05-30 01:53:35 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const port = 5000
+
+
+//Database Connection
+async function bootstrap() {
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/practice-Mongoose');
+    console.log("Database Connection Successfull")
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`)
+      })
+    }catch(err){
+        console.log(`Failed To Connect Database `,err);
+
+    }
+}
+bootstrap();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
